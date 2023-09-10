870ca879e575da3e21e2e17d0540fe5e30849ba2
Charchit Kasliwal <charchit.kasliwal@justeattakeaway.com>
2023-09-10 21:51:21 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/ghpracticethree/GhPracticeThreeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ghpracticethree
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GhPracticeThreeApplication
+
+fun main(args: Array<String>) {
+    runApplication<GhPracticeThreeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ghpracticethree/GhPracticeThreeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ghpracticethree
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GhPracticeThreeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
